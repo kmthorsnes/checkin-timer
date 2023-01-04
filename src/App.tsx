ef229b7a623703975d60f67ee8382b54bf54a5dd
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2023-01-04 13:54:23 +0100

    ✨ Create mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,84 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="bg-red flex">test</div>
-      <div className="card flex flex">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+interface Props {
+  // Props for komponenten
 }
 
-export default App;
+interface State {
+  checkInTime: Date | null;
+  difference7h35m: number | null;
+  difference8h: number | null;
+  differenceFromNow: {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  } | null;
+}
+
+const CheckInComponent: React.FC<Props> = (props) => {
+  const [checkInTime, setCheckInTime] = React.useState<Date | null>(null);
+  const [difference7h35m, setDifference7h35m] = React.useState<number | null>(null);
+  const [difference8h, setDifference8h] = React.useState<number | null>(null);
+  const [differenceFromNow, setDifferenceFromNow] = React.useState<State['differenceFromNow']>(null);
+
+  const handleCheckIn = () => {
+    const currentTime = new Date();
+    setCheckInTime(currentTime);
+    localStorage.setItem('checkInTime', currentTime.toISOString());
+
+    // Beregn differanse i millisekunder mellom nåværende tidspunkt og 7 timer og 35 minutter fra nå
+    const difference7h35m = (7 * 60 + 35) * 60 * 1000;
+    setDifference7h35m(difference7h35m);
+
+    // Beregn differanse i millisekunder mellom nåværende tidspunkt og 8 timer fra nå
+    const difference8h = 8 * 60 * 60 * 1000;
+    setDifference8h(difference8h);
+  };
+
+  React.useEffect(() => {
+    const storedCheckInTime = localStorage.getItem('checkInTime');
+    if (storedCheckInTime) {
+      setCheckInTime(new Date(storedCheckInTime));
+    }
+
+    if (checkInTime) {
+      const intervalId = setInterval(() => {
+        const currentTime = new Date();
+        const difference = currentTime.getTime() - checkInTime.getTime();
+        const seconds = Math.floor(difference / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        setDifferenceFromNow({
+          hours,
+          minutes: minutes % 60,
+          seconds: seconds % 60,
+        });
+      }, 1000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [checkInTime]);
+
+  return (
+    <div>
+      <label htmlFor="check-in-time">Når sjekket du inn:</label>
+      <input type="time" id="check-in-time" />
+      <button onClick={handleCheckIn}>Sjekk inn nå</button>
+      {checkInTime && (
+        <div>
+          <p>Du sjekket inn kl. {checkInTime.toLocaleTimeString()}</p>
+          {difference7h35m && <p>Differanse til 7 timer og 35 minutter: {difference7h35m} ms</p>}
+          {difference8h && <p>Differanse til 8 timer: {difference8h} ms</p>}
+          {differenceFromNow && (
+            <p>
+              Tidsforskjell fra nå: {differenceFromNow.hours} timer, {differenceFromNow.minutes} minutter,{' '}
+              {differenceFromNow.seconds} sekunder
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CheckInComponent;
